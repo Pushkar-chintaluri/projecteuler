74c48161a8c43387ea4e27dd8cb72c48ef68e456
Venkata Chintaluri <pushkar.chintaluri@gmail.com>
2018-06-13 17:44:33 -0400

    smallest multiple no code

---
+++ smallestmultiple.py
@@ -0,0 +1,22 @@
+# 1 - no multiples
+# 2 - no multiples
+# 3 - no multiples
+# 4 - 2 * 2
+# 5 - no multiples
+# 6 - 2 * 3
+# 7 - no multiples
+# 8 - 2 * 2 * 2
+# 9 - 3 * 3
+# 10 - 2 * 5
+# 11 - no multiples
+# 12 - 2 * 2 * 3
+# 13 - no multiples
+# 14 - 2 * 7
+# 15 - 2 * 3 * 5
+# 16 - 2 * 2 * 2 * 2
+# 17 - no multiples
+# 18 - 2 * 3 * 3
+# 19 - no multiples
+# 20 - 2 * 2 * 5
+
+print(1*2*3*2*5*7*2*3*11*13*2*17*19)
